b0ec1eafb705ee1ed6e2d6e5e0149e2726fb990c
Venus Rodin <VenusViolet1234@gmail.com>
2022-11-15 12:50:54 -1000

    Trying to get my JS to run. Just added the rest of my js

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
 const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
 const questionContainerElement = document.getElementById
 ('question-container')
+
 const questionElement = document.getElementById('question')
-const answerButtonsElement = document.getElementById('answer-buttons')
+const answerButtonsElement = document.getElementById
+('answer-buttons')
 
 let shuffledQuestions, currentQuestionIndex
 
+
 startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () = > {
+    currentQuestionIndex++
+    setNextQuestion()
+
+})
+
 
 function startGame() {
-    console.log('Started')
     startButton.classList.add('hide')
     shuffledQuestions = questions.sort(() => Math.random() - .5)
     currentQuestionIndex = 0
@@ -18,22 +27,90 @@ function startGame() {
 }
 
 function setNextQuestion() {
+    resetState()
     showQuestion(shuffledQuestions[currentQuestionIndex])
 }
 
 function showQuestion(question) {
-    questionElement.innertext = question.question
+    questionElement.innerText = question.question
+    questions.answers.forEach(answer => {
+        const button = document.createElement('button')
+        button.innerTexr = answer.text
+        button.classList.add('btn')
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener('click', selectAnswer)
+        answerButtonsElement.appendChild(button)
+
+    })
 
 }
 
-function selectAnswer() {
-
+function resetState() {
+    clearStatusClass(document.body)
+    nextButton.classList.add('hide')
+    while (answerButtonsElement.firstChild) {
+        answerButtonsElement.remove.firstChild
+        (answerButtonsElement.firstChild)
+    }
 }
 
-const questions = [
-    questions, "What is 1 + 1?" ,
-    answers, [
-        { text: '2' , correct: true },
-        { text: '3' , correct: false },
-    ]
+function selectAnswer(e) {
+    const selectedButton = e.target
+    const correct = selectedButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerButtonsElement.children).forEach(buton => {
+        setStatusClass(button, button.dataset.correct)
+    })
+    if (shuffledQuestions.length > currentQuestionIndex + 1) {
+        nextButton.classList.remove('hide')
+    } else {
+        startButton.innerText = 'Restart'
+        startButton.classList.remove('hide')
+    }
+}
+
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add('correct')
+    } else {
+        element.classList.add('wrong')
+    }
+}
+
+function clearStatusClass(element) {
+element.classList.remove('correct')
+element.classList.remove('wrong')
+}
+
+ questions = [
+    {
+         question: "What is 1 + 1?" ,
+         answers: [
+          { text: '2' , correct: true },
+          { text: '3' , correct: false },
+        ]
+    }
+]
+
+questions = [
+    {
+         question: "Which number is greater?" ,
+         answers: [
+          { text: '45' , correct: true },
+          { text: '9' , correct: false },
+        ]
+    }
+]
+
+questions = [
+    {
+         question: "How many inches are in a foot?" ,
+         answers: [
+          { text: '11' , correct: false },
+          { text: '12' , correct: true },
+        ]
+    }
 ]
