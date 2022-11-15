c00d363f020f67c2e8d9151bb8d03fb9015175e0
Venus Rodin <VenusViolet1234@gmail.com>
2022-11-15 12:10:35 -1000

    Added JS code to make the first question appear, shuffle questions, and am trying to figure out why it is saying null in the console

--- script.js
+++ script.js
@@ -1,15 +1,39 @@
 const startButton = document.getElementById('start-btn')
+const questionContainerElement = document.getElementById
+('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
+
+let shuffledQuestions, currentQuestionIndex
 
 startButton.addEventListener('click', startGame)
 
 function startGame() {
     console.log('Started')
+    startButton.classList.add('hide')
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
+    questionContainerElement.classList.remove('hide')
+    setNextQuestion()
 }
 
 function setNextQuestion() {
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+
+function showQuestion(question) {
+    questionElement.innertext = question.question
 
 }
 
 function selectAnswer() {
 
 }
+
+const questions = [
+    questions, "What is 1 + 1?" ,
+    answers, [
+        { text: '2' , correct: true },
+        { text: '3' , correct: false },
+    ]
+]
